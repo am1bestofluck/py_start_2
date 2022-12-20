0b9cb0531b16f8f62dfa4661089b96d4668a0b44
am_6733 <anton6733@gmail.com>
2022-12-20 16:02:14 +0200

    t3

--- homework_sem2.py
+++ homework_sem2.py
@@ -17,13 +17,12 @@ validate_input_float - принимаем от пользователя веще
 """
 
 
-__all__ = ['factorial_recursion']
+__all__ = ['factorial_recursion', 'validate_input_float']
 __version__ = "#2"
 __author__ = "anton6733@gmail.com"
 
 
 # standart imports
-import math
 import random
 import sys
 from typing import List, Tuple
@@ -34,22 +33,30 @@ from homework_sem1 import Break, validate_input
 
 def validate_input_float(
     limits: Tuple[int, int] = (- sys.maxsize, sys.maxsize),
-    accepts_zero=True, short_note: str = ""
-    ) -> float:
+    accepts_zero=False, short_note: str = ""
+) -> float:
     """
-    ~~~Харасим юзера.  ~~~
+    Принимаем с клавиатуры вещественное число
 
+    limits - предел допустимых значений (не меньше, не больше)
+
+    accepts_zero - если да - ноль не проходит
+
+    short_note - описание задачи вызова.
+
+    """
+    """
     correct - основа костыля TryParse из шарпа.  
     msg - приглашение ко вводу.  
     tmp - буфер для инпута.  
     output - выводное значение.  
     multiplier - костыль для отрицательных инпутов.  
-    accepts_zero - если False - ноль не пропускаем.  
-    short_note - описание задачи вызова.
+    accepts_zero - если False - ноль не пропускаем.
     separator - приводим разделитель к порядку
     """
-    # Была мысль принять float как два инта и потом их соединить, но 
-    # во-первых возникла проблема сжирания предварительных нулей, 
+
+    # Была мысль принять float как два инта и потом их соединить, но
+    # во-первых возникла проблема сжирания предварительных нулей,
     # во-вторых нужно было обращаться к вопросу разнознаковых частей.
     if short_note:
         print(short_note)
@@ -61,22 +68,22 @@ def validate_input_float(
     while not correct:
         tmp = input(msg)
         multiplier = -1 if tmp.startswith('-') else 1
-        tmp.replace(',',separator)
+        tmp = tmp.replace(',', separator)
         tmp = tmp.removeprefix('-')
         one_point = tmp.count(separator) == 1
         if not one_point:
-            msg ="better luck next time. Input number."
+            msg = "better luck next time. Input number."
             continue
         if tmp.startswith(separator):
-            tmp='0'+tmp
+            tmp = '0'+tmp
         if tmp.endswith(separator):
-            tmp+='0'
+            tmp += '0'
         try:
             output = float(tmp) * multiplier
         except:
-            msg="Input number?"
+            msg = "Input number?"
             continue
-        if output > limits[1] or output < limits [0]:
+        if output > limits[1] or output < limits[0]:
             msg = f"Between {limits[0]} and {limits[1]-1}. Try again."
         elif not accepts_zero and output == 0:
             msg = " Zero is not accepted. Try again"
@@ -85,7 +92,7 @@ def validate_input_float(
     return output
 
 
-def float_summarize_signs(number: float ) -> int:
+def float_summarize_signs(number: float) -> int:
     """
     Находим сумму знаков вещественного числа.
 
@@ -98,11 +105,13 @@ def float_summarize_signs(number: float ) -> int:
     """
     # математическое решение споткнулось о плавающуюююююююююююъй точку.
     output = 0
-    number_strf= str(number)
-    for  char in number_strf:
+    number_strf = str(number)
+    for char in number_strf:
         if char.isdigit():
             output += int(char)
     return output
+
+
 def factorial_recursion(base: int) -> int:
     """рекурсивно считаем факториал числа 
 
@@ -147,10 +156,41 @@ def lineup_factorials(N: int = 0, base: int = 1) -> List[int]:
     return output
 
 
-def lineup_floats(N: int) -> List[float]:
-    """создаёт список вещественных чисел, по формуле o = (i + 1/i) ** i
+def lineup_floats(N: int, base: int = 1) -> List[float]:
+    """выводит сумму списка вещественных чисел образованных по формуле 
+    o = (i + 1/i) ** i
+
+    N - верхний предел ряда
+
+    base - нижний предел ряда
     """
-    output = []
+    """
+    step_direction - направление шага, на случай base > N
+    N_mod - прыгаем вокруг range чтобы предельное значение попало в вывод
+    output - выводной список
+    calc_output_item- выносим расчётную часть
+    """
+
+    def calc_output_item(seed: int, round_to: int = 2) -> float:
+        """считаем вывод в соответсвии с ТЗ.
+
+        seed - int из внешнего счетчика 
+
+        round_to - знаков до отсечки"""
+        print(seed)
+        return round(ndigits=round_to,
+                     number=(1 + 1/seed) ** seed)
+    step_direction = 1 if (base < N) else -1
+    # определяемся с пределом и направлением шага списка
+    if base < N:
+        step_direction = 1
+        N_mod = N + 1
+    else:
+        step_direction = -1
+        N_mod = N - 1
+    #! определяемся с пределом и направлением шага списка
+    # идею с map подсмотрел на разборе.но это же вся суть- узнать новое.
+    output = list(map(calc_output_item,range(base, N_mod, step_direction)))
     return output
 
 
@@ -166,22 +206,30 @@ def shake_lineup(list_i: List[int]) -> list:
 
 def test_arrays_equality(
         seed_list: List[int], list_shuffled: List[int]
-        ) -> bool:
+) -> bool:
     return sorted(seed_list) == sorted(list_shuffled)
 
 
 def main():
-    print( float_summarize_signs.__doc__)
-    print(float_summarize_signs(validate_input_float(short_note="float!")))
+    def t1():
+        print(float_summarize_signs.__doc__)
+        print(float_summarize_signs(validate_input_float(short_note="float!")))
+        Break()
 
-    # print(lineup_factorials.__doc__)
-    # print(lineup_factorials(validate_input(short_note="Input N?")))
-    # Break()
-    # print(float_summarize_signs.__doc__)
-    # print(float_summarize_signs(
-    #     validate_input(short_note="Input whole part?"),
-    #     validate_input(accepts_zero=False, short_note="Input fractured part?")))
+    def t2():
+        print(lineup_factorials.__doc__)
+        print(lineup_factorials(validate_input(short_note="Input N?")))
+        Break()
     
+    def t3():
+        print(lineup_floats.__doc__)
+        print(lineup_floats(
+            validate_input(accepts_zero=False,
+                        short_note="N? int")
+        ))
+    # t1()5
+    t3()
+
 
 if __name__ == "__main__":
     main()
